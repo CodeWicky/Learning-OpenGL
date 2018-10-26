69ddb8b09373266fd3ce25aeeba1700be05f9c05
Wicky <codewicky@163.com>
2018-10-26 15:25:55 +0800

    注释

--- Work/OpenGL_Template/OpenGL_Template/main.cpp
+++ Work/OpenGL_Template/OpenGL_Template/main.cpp
@@ -5,6 +5,8 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+GLFWwindow * configOpenGL ();
+void finishiRenderLoop ();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -12,75 +14,85 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-    
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+    GLFWwindow * window = configOpenGL();
+    if (NULL == window) {
         return -1;
     }
     
-    // render loop
-    // -----------
+    ///Render loop
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
+        
         processInput(window);
         
-        // render
-        // ------
+        ///设置清屏颜色
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        ///清屏
         glClear(GL_COLOR_BUFFER_BIT);
         
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        ///交换颜色缓冲
         glfwSwapBuffers(window);
+        ///拉取用户事件
         glfwPollEvents();
     }
     
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
+    finishiRenderLoop();
+    
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+GLFWwindow* configOpenGL () {
+    ///初始化glfw
+    glfwInit();
+    
+    ///设置版本号
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
+    ///设置核心模式
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
+    ///设置对Mac OS X的兼容
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    ///创建window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return NULL;
+    }
+    ///将window设置成当前上下文
+    glfwMakeContextCurrent(window);
+    ///设置窗口事件更新触发的回调
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    ///初始化GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return NULL;
+    }
+    return window;
+}
+
+void finishiRenderLoop () {
+    ///释放窗口资源
+    glfwTerminate();
+}
+
+///处理输入
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+///窗口事件更新回调
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
+    ///设置视口大小
     glViewport(0, 0, width, height);
 }
