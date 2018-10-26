e7858b4062e8898223b57a85c4da742c14aa67d9
Wicky <codewicky@163.com>
2018-10-26 12:49:26 +0800

    Demo

--- Work/LearnOpenGLES/LearnOpenGLES/ViewController.m
+++ Work/LearnOpenGLES/LearnOpenGLES/ViewController.m
@@ -7,18 +7,44 @@
 //
 
 #import "ViewController.h"
+#import <GLKit/GLKit.h>
 
-@interface ViewController ()
 
-@property (nonatomic ,strong) EAGLContext * context;
+@interface ViewController ()<GLKViewDelegate>
+@property (nonatomic ,strong) EAGLContext * mContext;
+
+@property (nonatomic ,strong) GLKView * view;
 
 @end
 
 @implementation ViewController
 
+-(void)loadView {
+    [super loadView];
+    self.view = [[GLKView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view.delegate = self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self configGLContext];
+}
+
+-(void)configGLContext {
+    self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; //2.0，还有1.0和3.0
+    self.view.context = self.mContext;
+    self.view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;  //颜色缓冲区格式
+    [EAGLContext setCurrentContext:self.mContext];
+}
+
+-(void)doRenderTask {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+#pragma mark --- glkView Delegate ---
+-(void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    [self doRenderTask];
 }
 
 
